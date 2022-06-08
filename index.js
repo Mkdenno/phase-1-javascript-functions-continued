d7d0350a7e5b39cee8060f5191464b76534f4e83
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-08 12:32:35 +0300

    Completed

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // code your solution here
-
 function saturdayFun(activity="roller-skate"){
-    return(`This Saturday, I want to ${activity}!`);
+    return (`This Saturday, I want to ${activity}!`)
 }
-function mondayWork(activity="go to the office"){
-    return `This Monday, I will ${activity}.`
-
+function mondayWork(task="go to the office"){
+    return `This Monday, I will ${task}.`
 }
 function wrapAdjective(result="*"){
-    return function wrapAdj(emphatic="a hard worker"){
-    return `You are ${result}${emphatic}${result}!`;
+    return function wrapadj(emphatic="a hard worker"){
+        return `You are ${result}${emphatic}${result}!`
     }
 }
